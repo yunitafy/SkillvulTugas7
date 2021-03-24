81e8c3cd91b0f56cf00ad4251d3f459969630958
yunitafy <yunitafy9@gmail.com>
2021-03-24 03:55:00 -0700

    first commit

---
+++ tugas7.js
@@ -0,0 +1,36 @@
+//tugas ke 7
+//Array
+
+//soal nomor 1 
+
+let pekerjaanIT=
+ ["Front end developer","Back end developer","Web developer","Software developer",design ",
+];
+
+document.write(`5 pekerjaan bidang IT`);
+document.write(`<ol>`);
+perkerjaanIT.array.forEach(pekerjaanIT => {
+    Documen.write(`</ol>`)
+});
+
+//soal nomor 2
+const team=[`ka Toriq`, `ka Danu`, `ka Luthfi`, `Yunita`];
+const timbaru = tim.splice(0,2);
+
+timke1 = `ka Thoriq (leader)`;
+timke2 = `ka Danu(leader)`;
+
+//soal nomor 3
+let arr =[3,5,7,9,11];
+let newarr=arr.map((numb)=>{
+    return num*5;
+});
+
+console.log{newarr};
+
+//soal nomor 4
+const arrayke1=[`math`,`English`,`Programming`];
+const arrayke2=[`georaphy`,`spanish`,`programming`];
+
+console.log(matpel1);
+console.log(mapel2);
